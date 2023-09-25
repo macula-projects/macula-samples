efcb2d3c4b78d596fbed6e6da2b413a7bb63768a
Rain Wang <rainsoft@163.com>
2023-09-25 14:01:42 +0800

    代码整理2 应用编辑bug修改

--- macula-samples-api/macula-samples-service1-api/src/main/java/dev/macula/samples/service1/query/ApplicationPageQuery.java
+++ macula-samples-api/macula-samples-service1-api/src/main/java/dev/macula/samples/service1/query/ApplicationPageQuery.java
@@ -20,10 +20,12 @@ package dev.macula.samples.service1.query;
 import dev.macula.boot.base.BasePageQuery;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 应用分页查询实体
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class ApplicationPageQuery extends BasePageQuery {
 
